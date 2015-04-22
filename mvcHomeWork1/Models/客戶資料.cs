dcb4e0cc41f7596070167f8ef2c84b5262d1ed7e
Bob <chihchen119@gmail.com>
2015-04-22 13:07:13 +0800

    對於 Create 與 Edit 表單要套用欄位驗證 (必填或 Email 格式驗證)

--- mvcHomeWork1/Models/客戶資料.cs
+++ mvcHomeWork1/Models/客戶資料.cs
@@ -11,6 +11,7 @@ namespace mvcHomeWork1.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class 客戶資料
     {
@@ -21,11 +22,14 @@ namespace mvcHomeWork1.Models
         }
     
         public int Id { get; set; }
+        [Required]
         public string 客戶名稱 { get; set; }
         public string 統一編號 { get; set; }
         public string 電話 { get; set; }
         public string 傳真 { get; set; }
         public string 地址 { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
     
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
